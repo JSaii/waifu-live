a65de921cd04935f9e68bab2f58cd353e10830a3
JSaii <38206432+JSaii@users.noreply.github.com>
2025-06-21 23:53:24 +0900

    Create send chat functionalities. Send chat queues custom message on top of the monitor loop.

--- app/monitor.py
+++ app/monitor.py
@@ -2,6 +2,7 @@ import pyautogui
 import openai_api
 import utils
 from overlay import run_overlay
+from send_chat import run_chatbox
 import keyboard
 from multiprocessing import Process, Queue
 import time
@@ -50,19 +51,31 @@ messages = [{
     "content":today_info
 }]
 
-def monitor(queue: Queue):
+def monitor(queue: Queue, send_chat_queue: Queue):
      while running:
-        captureScreen(screenshot_save_path)
-        print("Screenshot captured...")
-        img_message = openai_api.build_message(text="...", role="user", image_path=screenshot_save_path)
-        messages.append(img_message)
+        is_chatting = False
+        if not send_chat_queue.empty():
+            is_chatting = True
+            text = send_chat_queue.get()
+            print(f"Sending: {text}")
+            message = openai_api.build_message(text=text)
+            messages.append(message)
+        else:
+            print("Empty queue")
+        
+        if not is_chatting:
+            captureScreen(screenshot_save_path)
+            print("Screenshot captured...")
+            img_message = openai_api.build_message(text="...", role="user", image_path=screenshot_save_path)
+            messages.append(img_message)
 
         print("Generating response...")
         response = openai_api.get_response(messages)
         response_message = openai_api.build_message(role="assistant", text=response)
 
-        messages.pop()
-        messages.append(openai_api.build_message(text="..., image:[Image removed to save api credits. Continue with available context.]"))
+        if not is_chatting:
+            messages.pop()
+            messages.append(openai_api.build_message(text="..., image:[Image removed to save api credits. Continue with available context.]"))
 
         messages.append(response_message)
 
@@ -83,11 +96,19 @@ if __name__ == "__main__":
     overlay_proc.start()
     print(f"Initializing overlay...[Complete]")
 
+    print(f"Initializing send chatbox...")
+    send_chat_queue = Queue()
+    send_chat_proc = Process(target=run_chatbox, args=(send_chat_queue,))
+    send_chat_proc.start()
+    print(f"Initializing send chatbox...[Complete]")
+
     print(f"Initializing tts...")
     tts_engine.init_tts()
     print(f"Initializing tts...[Complete]")
     print(f"Starting monitor...")
-    monitor(queue)
+    monitor(queue, send_chat_queue)
 
     overlay_proc.terminate()
+    send_chat_proc.terminate()
     overlay_proc.join()
+    send_chat_proc.join()
